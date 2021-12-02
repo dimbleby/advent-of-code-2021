71a2baead2aeef0ac38a41f47223c766e86b5296
David Hotham <david.hotham@metaswitch.com>
2021-12-02 16:04:21 +0000

    format

--- src/day01.rs
+++ src/day01.rs
@@ -1,9 +1,6 @@
 pub(crate) fn day01() {
     let input = std::fs::read_to_string("data/day01.txt").unwrap();
-    let readings: Vec<usize> = input
-        .lines()
-        .map(|line| line.parse().unwrap())
-        .collect();
+    let readings: Vec<usize> = input.lines().map(|line| line.parse().unwrap()).collect();
 
     let increases = count_increases(&readings, 1);
     println!("Part one answer is {}", increases);
