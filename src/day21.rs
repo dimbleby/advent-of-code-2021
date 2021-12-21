85c88ff99435cb9137cf0bc75c2882f4e3900532
David Hotham <david.hotham@metaswitch.com>
2021-12-21 12:19:50 +0000

    Day 21

---
+++ src/day21.rs
@@ -0,0 +1,90 @@
+use std::collections::HashMap;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+struct Player {
+    position: usize,
+    score: usize,
+}
+
+impl Player {
+    fn new(position: usize) -> Self {
+        Self { position, score: 0 }
+    }
+
+    fn roll(&mut self, value: usize) {
+        self.position += value;
+        self.position %= 10;
+        if self.position == 0 {
+            self.position = 10
+        };
+        self.score += self.position;
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+struct GameState {
+    player1: Player,
+    player2: Player,
+}
+
+impl GameState {
+    fn new(player1: Player, player2: Player) -> Self {
+        Self { player1, player2 }
+    }
+
+    fn roll(&mut self, value: usize, player1_turn: bool) {
+        let player = if player1_turn {
+            &mut self.player1
+        } else {
+            &mut self.player2
+        };
+        player.roll(value);
+    }
+}
+
+pub(crate) fn day21() {
+    let mut player1 = Player::new(8);
+    let mut player2 = Player::new(2);
+    let mut dice = 1;
+    'outer: loop {
+        for player in [&mut player1, &mut player2] {
+            player.roll(dice + dice + 1 + dice + 2);
+            dice += 3;
+            if player.score >= 1000 {
+                break 'outer;
+            }
+        }
+    }
+    let loser_score = player1.score.min(player2.score);
+    println!("Part one answer is {}", (dice - 1) * loser_score);
+
+    let player1 = Player::new(8);
+    let player2 = Player::new(2);
+    let mut player1_wins = 0;
+    let mut player2_wins = 0;
+
+    let game = GameState::new(player1, player2);
+    let mut worlds = hashmap! { game => 1u64 };
+
+    let mut player1_turn = true;
+    while !worlds.is_empty() {
+        let mut new_worlds = HashMap::default();
+        for (state, count) in &worlds {
+            for (roll, ways) in [(3, 1u64), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
+                let new_count = ways * count;
+                let mut new_state = *state;
+                new_state.roll(roll, player1_turn);
+                if new_state.player1.score >= 21 {
+                    player1_wins += new_count;
+                } else if new_state.player2.score >= 21 {
+                    player2_wins += new_count;
+                } else {
+                    *new_worlds.entry(new_state).or_default() += new_count;
+                }
+            }
+        }
+        player1_turn = !player1_turn;
+        worlds = new_worlds;
+    }
+    println!("Part two answer is {}", player1_wins.max(player2_wins));
+}
--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ mod day17;
 mod day18;
 mod day19;
 mod day20;
+mod day21;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
@@ -52,6 +53,7 @@ fn main() {
         18 => day18::day18(),
         19 => day19::day19(),
         20 => day20::day20(),
+        21 => day21::day21(),
         _ => println!("Unimplemented day: {}", args.day),
     }
 }
